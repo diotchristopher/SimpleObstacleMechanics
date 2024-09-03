1fdc98dc50a993f8821410efb1ee0df2a4ca5d49
Chris .D <178487367+diotchristopher@users.noreply.github.com>
2024-09-03 15:13:46 +0200

    Add project files.

---
+++ Source/SimpleObsMechanics/Public/SimpleObsMechanicsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimpleObsMechanicsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ASimpleObsMechanicsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ASimpleObsMechanicsGameMode();
+};
+
+
+
